159dacd088d036e203e53399ec777f1ca0834b18
flashbat51 <georgeadomakojnr51@aol.com>
2023-03-29 15:34:14 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *  * *rot13 - encode string with rot13
+ *   * @rot: encode rot 13
+ *    * Return: decode result
+ */
+char *rot13(char *rot)
+{
+	int i = 0, j = 0;
+	int code[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z'};
+	int decode[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm'};
+
+	for (i = 0; rot[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (rot[i] == code[j])
+			{
+				rot[i] = decode[j];
+				break;
+			}
+		}
+	}
+	return (rot);
+}
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  * print_number - print a integer
+ *   * @n: number to print
+ *    * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int dec, count, i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = n * -1;
+	}
+	else
+		i = n;
+	dec = i;
+	count = 1;
+	while (dec > 9)
+	{
+		dec /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+		_putchar(((i / count) % 10) + '0');
+}
