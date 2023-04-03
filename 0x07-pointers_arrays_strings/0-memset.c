38c6319531b24d96fb9515bbf4342e63a8770d23
flashbat51 <georgeadomakojnr51@aol.com>
2023-04-03 12:56:38 -0700

    additional pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,33 +1,27 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- *  * _memset - Fills memory with a constant byte
- *   * @s: buffer array
- *    * @b: constant byte
- *     * @n: number of bytes of memory area to fill
- *      * Description: Fill the first `n` bytes of
- *       * the memory area pointed to by `s`
- *        * with the constant byte `b`.
- *         * Return: Pointer to memory area `s`
+ *  _memset - Fills memory with a constant byte
+ *  @s: buffer array
+ *  @b: constant byte
+ *  @n: number of bytes of memory area to fill
+ *  Description: Fill the first `n` bytes of
+ *  the memory area pointed to by `s`
+ *  with the constant byte `b`.
+ *  Return: Pointer to memory area `s`
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	for (int i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
-}
+int c;
 
-int main(void)
+c = 0;
+while (n > 0)
 {
-	char buffer[10] = {0};
+	s[c] = b;
 
-	memset(buffer, 'A', 5);
-
-	printf("%s\n", buffer); /*  Output: AAAAA */
-
-	return (0);
+	c++;
+	n--;
+}
+return (s);
 }
