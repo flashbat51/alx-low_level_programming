13173160ed0dfb262c3b926968bf1730b11ebf7c
flashbat51 <georgeadomakojnr51@aol.com>
2023-04-03 18:17:57 -0700

    mending 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 
 /**
- * _strstr - locate a substring
- * @haystack: string
- * @needle: string substring
- * Return: Pointer to beginning of substring, or NULL if not found.
+ *  * _strstr - locate a substring
+ *   * @haystack: string
+ *    * @needle: string substring
+ *     * Return: Pointer to beginning of substring, or NULL if not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int a, b, c;
+	int i, j, c;
 
-	a = 0;
+	i = 0;
 	c = 0;
-	while (haystack[a] != '\0')
+	while (haystack[i] != '\0')
 	{
 		j = 0;
-		while (needle[b + c] != '\0' && haystack[a + c] != '\0'
-				&& needle[b + c] == haystack[a + c])
+		while (needle[j + c] != '\0' && haystack[i + c] != '\0'
+				&& needle[j + c] == haystack[i + c])
 		{
-			if (haystack[a + c] != needle[b + c])
+			if (haystack[i + c] != needle[j + c])
 				break;
 			c++;
 		}
-		if (needle[b + c] == '\0')
-			return (&haystack[a]);
-		b++;
-		a++;
+		if (needle[j + c] == '\0')
+			return (&haystack[i]);
+		j++;
+		i++;
 	}
 
 	return (NULL);
