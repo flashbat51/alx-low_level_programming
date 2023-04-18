25ba60271acc07a95554c437e9c74d34877f6d99
flashbat51 <georgeadomakojnr51@aol.com>
2023-04-17 18:27:20 -0700

    updating dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,7 @@
  * @name: The dog's name
  * @age: The dog's age
  * @owner: The dog's owner
- * Description: User defined variable in C, named dog that stores 
+ * Description: User defined variable in C, named dog that stores
  * some information about any dog, consisting of; the name, age and owner.
  */
 struct dog
