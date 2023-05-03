632f99a70a4fe9bf78005750acf796305c28014d
flashbat51 <georgeadomakojnr51@aol.com>
2023-05-02 18:52:50 -0700

    adding advance task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listp - frees linked list
+ * @head: head of list.
+ * Return: no return.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
