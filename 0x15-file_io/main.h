4f5b3288ad43a878f8cdcf4f4bdfa71425233f9f
flashbat51 <georgeadomakojnr51@aol.com>
2023-05-08 20:01:19 -0700

    updating main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,10 @@
 #ifndef IO_
 #define IO_
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 int _strlen(const char *s);
 int _putchar(char c);
 int append_text_to_file(const char *filename, char *text_content);
